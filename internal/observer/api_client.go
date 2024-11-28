5da66e8e39207141a5c84c1c8c8a2a969cc49d93
therealmal <therealmal@192.168.1.2>
2024-11-28 19:02:11 +0300

    fix: minor fix init check

--- internal/observer/api_client.go
+++ internal/observer/api_client.go
@@ -11,26 +11,26 @@ import (
 
 func NewAPIClient(ctx context.Context, cfg *config.Config) (ton.APIClientWrapped, error) {
 	client := liteclient.NewConnectionPool()
+	globalConfig, err := liteclient.GetConfigFromUrl(ctx, config.GlobalConfigURL[cfg.IsTestnet])
+	if err != nil {
+		return nil, err
+	}
 	if !cfg.Public {
-		if err := client.AddConnection(ctx, cfg.LiteserverHost, cfg.LiteserverKey); err != nil {
+		if err = client.AddConnection(ctx, cfg.LiteserverHost, cfg.LiteserverKey); err != nil {
 			return nil, err
 		}
 
 	} else {
-		globalConfig, err := liteclient.GetConfigFromUrl(context.Background(), config.GlobalConfigURL[cfg.IsTestnet])
-		if err != nil {
-			return nil, err
-		}
-		err = client.AddConnectionsFromConfig(context.Background(), globalConfig)
+		err = client.AddConnectionsFromConfig(ctx, globalConfig)
 		if err != nil {
 			return nil, err
 		}
 	}
 	api := ton.NewAPIClient(client, ton.ProofCheckPolicyFast).WithRetry()
-	// api.SetTrustedBlockFromConfig(globalConfig)
+	api.SetTrustedBlockFromConfig(globalConfig)
 
 	log.Println("fetching and checking proofs since config init block ...")
-	_, err := api.CurrentMasterchainInfo(context.Background())
+	_, err = api.CurrentMasterchainInfo(ctx)
 	if err != nil {
 		return nil, err
 	}
